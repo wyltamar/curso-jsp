b7e694bc795ec71d702cdf4bea8b5b8f3e937dc9
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2022-02-02 21:15:37 -0300

    Melhoria na identação do código

--- curso-jsp/src/main/java/servelets/ServletUsuarioController.java
+++ curso-jsp/src/main/java/servelets/ServletUsuarioController.java
@@ -12,66 +12,65 @@ import javax.servlet.http.HttpServletResponse;
 import dao.DAOUsuarioRepository;
 import model.ModelLogin;
 
-
 @WebServlet("/ServletUsuarioController")
 public class ServletUsuarioController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
-	String msg = "Registro salvo com sucesso!";
-    
-	private DAOUsuarioRepository daoUsuarioRepository = new DAOUsuarioRepository();
-   
-    public ServletUsuarioController() {
-       
-    }
 
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	String msg = "Registro salvo com sucesso!";
+
+	private DAOUsuarioRepository daoUsuarioRepository = new DAOUsuarioRepository();
+
+	public ServletUsuarioController() {
+
 	}
 
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		try {
-		String id = request.getParameter("id");
-		String nome = request.getParameter("nome");
-		String email = request.getParameter("email");
-		String login = request.getParameter("login");
-		String senha = request.getParameter("senha");
-		
-		ModelLogin modelLogin = new ModelLogin();
-		modelLogin.setId(id != null && !id.isEmpty() ? Long.parseLong(id): null);
-		modelLogin.setNome(nome);
-		modelLogin.setEmail(email);
-		modelLogin.setLogin(login);
-		modelLogin.setSenha(senha);
-		
+			String id = request.getParameter("id");
+			String nome = request.getParameter("nome");
+			String email = request.getParameter("email");
+			String login = request.getParameter("login");
+			String senha = request.getParameter("senha");
+
+			ModelLogin modelLogin = new ModelLogin();
+			modelLogin.setId(id != null && !id.isEmpty() ? Long.parseLong(id) : null);
+			modelLogin.setNome(nome);
+			modelLogin.setEmail(email);
+			modelLogin.setLogin(login);
+			modelLogin.setSenha(senha);
+
 			if (daoUsuarioRepository.validarLogin(modelLogin.getLogin()) && modelLogin.getId() == null) {
-			msg = "Já existe usuário com este login, informe um outro login!";
-		}else {
-			
-			if(modelLogin.getId() != null) {
-				modelLogin = daoUsuarioRepository.atualizarUsuario(modelLogin);
-				msg = "Registro atualizado com sucesso!";
-			}else {
-			modelLogin = daoUsuarioRepository.gravarUsuario(modelLogin);
-			msg = "Registro salvo com sucesso!";
+				msg = "Já existe usuário com este login, informe um outro login!";
+			} else {
+
+				if (modelLogin.getId() != null) {
+					modelLogin = daoUsuarioRepository.atualizarUsuario(modelLogin);
+					msg = "Registro atualizado com sucesso!";
+				} else {
+					modelLogin = daoUsuarioRepository.gravarUsuario(modelLogin);
+					msg = "Registro salvo com sucesso!";
+				}
 			}
-		}
-		
-		request.setAttribute("msg", msg);
-		
-		request.setAttribute("modelLogin", modelLogin);
-		request.getRequestDispatcher("principal/usuario.jsp").forward(request, response);
-		
-		}catch(Exception e) {
+
+			request.setAttribute("msg", msg);
+
+			request.setAttribute("modelLogin", modelLogin);
+			request.getRequestDispatcher("principal/usuario.jsp").forward(request, response);
+
+		} catch (Exception e) {
 			e.printStackTrace();
 			RequestDispatcher redirect = request.getRequestDispatcher("erro.jsp");
 			request.setAttribute("msg", e.getMessage());
 			redirect.forward(request, response);
 		}
-		
+
 	}
 
 }
